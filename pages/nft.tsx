13394d68e3165db2e92288211190395748272afc
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2023-06-06 15:45:15 -0300

    nft random image box

--- pages/nft.tsx
+++ pages/nft.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { StateContext } from "@/context/StateContext";
 import Layout from "@/components/layout";
 import { Inter } from "next/font/google";
@@ -6,16 +6,47 @@ import SSButton from "@/components/ssButton";
 import { mint } from "@/contracts_integrations/nft";
 
 import { ethers } from "ethers";
+import LayoutClean from "@/components/layoutClean";
+import Image from "next/image";
 const inter = Inter({
   subsets: ["latin"],
 });
 
+const images = [
+  "/nfts/1.png",
+  "/nfts/2.png",
+  "/nfts/3.png",
+  "/nfts/4.png",
+  "/nfts/5.png",
+  "/nfts/6.png",
+  "/nfts/7.png",
+  "/nfts/8.png",
+];
+
 export default function Nft() {
   const { signer } = useContext(StateContext);
 
+  const [image, setImage] = useState(
+    images[Math.floor(Math.random() * images.length)]
+  );
+  const [imageKey, setImageKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageKey((prevKey) => prevKey + 1);
+      setImage(images[Math.floor(Math.random() * images.length)]);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  console.log(Math.floor(Math.random() * images.length));
+
   return (
-    <Layout>
-      <div className="px-6 lg:px-8 xl:px-20">
+    <LayoutClean>
+      <div className="flex w-full justify-between items-center px-6 lg:px-8 xl:px-20">
         {/* TOP CONTENT */}
         <div className="pt-10 lg:pt-24 lg:max-w-[750px] lg:h-[630px]">
           <h1 className="text-[58px] lg:text-[68px] font-black leading-[62px] tracking-wide">
@@ -32,7 +63,21 @@ export default function Nft() {
             </div>
           )}
         </div>
+
+        {/* TOP SIDE CONTENT */}
+        <div className="">
+          <div className="flex justify-center items-center w-[500px] h-[500px] bg-white rounded-[8px] relative">
+            <Image
+              key={imageKey}
+              src={image}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              alt=""
+              className="scale-[0.95] rounded-[8px]"
+            />
+          </div>
+        </div>
       </div>
-    </Layout>
+    </LayoutClean>
   );
 }
