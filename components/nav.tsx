0b9df6ab29aa5ac408d0407b9457a4daa82dea02
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2024-04-25 11:56:21 -0300

    ca contract link

--- components/nav.tsx
+++ components/nav.tsx
@@ -20,7 +20,7 @@ export default function Nav() {
     <div
       className={`h-20 px-2 lg:px-8 flex flex-col md:flex-row items-center justify-between mt-5 z-10 ${inter.className} h-max`}
     >
-      <div className="flex items-center gap-9 font-bold text-lg 2xl:text-xl">
+      <div className="flex items-center gap-6 font-bold text-lg 2xl:text-xl">
         <Link
           href="/"
           className="transition-all hover:opacity-75"
@@ -33,7 +33,7 @@ export default function Nav() {
             width={0}
             height={0}
             alt="logo"
-            className=" mt-[-5px] drop-shadow-xl w-[400px] lg:w-[320px] h-[100px] px-3 lg:px-0"
+            className=" mt-[-5px] drop-shadow-xl w-[400px] lg:w-[300px] h-[80px] px-3 lg:px-0"
           />
         </Link>
         {NAV.map((item, index) => (
@@ -52,16 +52,25 @@ export default function Nav() {
           </Link>
         ))}
       </div>
-      <div className="flex gap-5 px-5 lg:px-0">
+      <div className="flex flex-col md:flex-row  gap-5 px-5 lg:px-0 h-max">
         {(page === Page.nft || query.ido !== "") && (
-          <ConnectButton showBalance={false} />
+          <div className="flex self-center h-14">
+            <ConnectButton showBalance={false} />
+          </div>
         )}
 
-        <div className="hidden xl:flex">
+        <SSButton
+          isLink
+          target="blank"
+          href="https://basescan.org/token/0xed1779845520339693CDBffec49a74246E7D671b"
+        >
+          CA: 0xe...71b
+        </SSButton>
+        {/* <div className="hidden xl:flex">
           <SSButton isLink target="blank" href="https://v1.samuraistarter.com">
             V1
           </SSButton>
-        </div>
+        </div> */}
       </div>
     </div>
   );
