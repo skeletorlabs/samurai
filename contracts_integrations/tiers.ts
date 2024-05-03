ac29c6d7c2854e84e26a5ac41489e579b426c30b
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2024-05-03 13:30:15 -0300

    updates

--- contracts_integrations/tiers.ts
+++ contracts_integrations/tiers.ts
@@ -2,18 +2,22 @@ import { SAM_NFT } from "@/utils/constants";
 import { Contract, JsonRpcSigner } from "ethers";
 import { NFTS_ABI } from "./abis";
 
-export async function isSamHolder(signer: JsonRpcSigner) {
+export async function checkNftHolding(signer: JsonRpcSigner) {
   const address = await signer.getAddress();
   const contract = new Contract(SAM_NFT, NFTS_ABI, signer);
   const balance = await contract.balanceOf(address);
 
-  return balance > 0;
+  return balance;
 }
-export async function isLockingSam(signer: JsonRpcSigner) {}
-export async function isStakingLP(signer: JsonRpcSigner) {}
+export async function lockingSam(signer: JsonRpcSigner) {
+  return 0;
+}
+export async function stakingLP(signer: JsonRpcSigner) {
+  return 0;
+}
 
 export async function getTierByWallet(signer: JsonRpcSigner) {
-  const isHolder = await isSamHolder(signer);
-  const isLocking = await isLockingSam(signer);
-  const isStaking = await isStakingLP(signer);
+  const holding = await checkNftHolding(signer);
+  const locking = await lockingSam(signer);
+  const staking = await stakingLP(signer);
 }
