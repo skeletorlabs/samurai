5654ca1712ac18294e1e084992e11b1e633a0c2c
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2024-10-14 09:27:25 -0300

    update xr one end date

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -82,7 +82,7 @@ export const IDO_LIST: IDO[] = [
     price: "637",
     registrationStartsAt: 1719410792,
     participationStartsAt: 1728475200,
-    participationEndsAt: 1729080000,
+    participationEndsAt: 1729684800,
     publicParticipationStartsAt: 0,
     publicParticipationEndsAt: 0,
     simplified: true,
