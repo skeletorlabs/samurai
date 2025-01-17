6236a07032bf235e29f6900fa1e78db64fe22658
Lucas Silveira <29531+lucasfernandes@users.noreply.github.com>
2025-01-17 10:31:28 -0300

    begin

---
+++ app/contracts_integrations/lpStaking.ts
@@ -0,0 +1,380 @@
+import { ethers, formatEther } from "ethers";
+import { ERC20_ABI, LP_STAKING_ABI } from "./abis";
+import handleError from "@/app/utils/handleErrors";
+import { balanceOf } from "./balanceOf";
+import checkApproval from "./check-approval";
+import { notificateTx } from "@/app/utils/notificateTx";
+
+const BASE_RPC_URL = process.env.NEXT_PUBLIC_BASE_RPC_HTTPS as string;
+
+async function getContract(signer?: ethers.Signer) {
+  try {
+    const provider = new ethers.JsonRpcProvider(BASE_RPC_URL);
+    const contractAddress = "0x....";
+    const contract = new ethers.Contract(
+      contractAddress,
+      LP_STAKING_ABI,
+      signer || provider
+    );
+
+    return contract;
+  } catch (e) {
+    handleError({ e: e, notificate: true });
+  }
+}
+
+// OVERALL INFOS
+
+export async function checkIsPaused() {
+  try {
+    const contract = await getContract();
+    const isPaused = await contract?.paused();
+    return isPaused;
+  } catch (e) {
+    handleError({ e: e, notificate: true });
+  }
+}
+
+export async function generalInfo() {
+  // uint256 public constant MAX_STAKES_PER_WALLET = 5; // Maximum number of stakes per wallet
+  // uint256 public constant MAX_AMOUNT_TO_STAKE = 10_000 ether; // 10,000 LP tokens
+  // uint256 public constant CLAIM_DELAY_PERIOD = 5 minutes; // 5 minutes
+
+  // // Define stake periods (in seconds)
+  // uint256 public constant THREE_MONTHS = 3 * 30 days; // 3 months
+  // uint256 public constant SIX_MONTHS = 6 * 30 days; // 6 months
+  // uint256 public constant NINE_MONTHS = 9 * 30 days; // 9 months
+  // uint256 public constant TWELVE_MONTHS = 12 * 30 days; // 12 months
+
+  // ERC20 public immutable lpToken;
+  // ERC20 public immutable rewardsToken;
+  // IGauge public immutable gauge;
+  // IPoints private immutable iPoints;
+  // uint256 public pointsPerToken;
+
+  // uint256 public totalStaked;
+  // uint256 public totalWithdrawn;
+
+  try {
+    const contract = await getContract();
+
+    const owner = await contract?.owner();
+    const isPaused = await contract?.paused();
+
+    const maxStakesPerWallet = Number(await contract?.MAX_STAKES_PER_WALLET());
+    const maxAmountToStake = Number(
+      ethers.formatEther(await contract?.MAX_AMOUNT_TO_STAKE())
+    );
+    const claimDelayPeriod = Number(await contract?.CLAIM_DELAY_PERIOD());
+    const threeMonths = await contract?.THREE_MONTHS();
+    const sixMonths = await contract?.SIX_MONTHS();
+    const nineMonths = await contract?.NINE_MONTHS();
+    const twelveMonths = await contract?.TWELVE_MONTHS();
+
+    const totalStaked = Number(
+      ethers.formatEther(await contract?.totalStaked())
+    );
+    const totalWithdrawn = Number(
+      ethers.formatEther(await contract?.totalWithdrawn())
+    );
+
+    const periods = [
+      { title: "3 Months", value: threeMonths },
+      { title: "6 Months", value: sixMonths },
+      { title: "9 Months", value: nineMonths },
+      { title: "12 Months", value: twelveMonths },
+    ];
+
+    // return {
+    //   owner,
+    //   isPaused,
+    //   minToLock,
+    //   periods,
+    // };
+  } catch (e) {
+    handleError({ e: e, notificate: true });
+  }
+}
+
+export async function getMultiplierByPeriod(period: number) {
+  const contract = await getContract();
+
+  const multiplier = Number(
+    ethers.formatEther(await contract?.multipliers(period))
+  );
+
+  return multiplier;
+}
+
+export async function getEstimatedPoints(amount: string, period: number) {
+  const multiplier = await getMultiplierByPeriod(period);
+
+  return Number(amount) * multiplier;
+}
+
+// USER INFOS
+
+export type LockInfo = {
+  lockIndex: number;
+  lockedAmount: number;
+  withdrawnAmount: number;
+  lockedAt: number;
+  unlockTime: number;
+  lockPeriod: number;
+  multiplier: number;
+  points: number;
+};
+
+export type UserInfo = {
+  locks: LockInfo[];
+  totalLocked: number;
+  totalPoints: number;
+  samBalance: number;
+};
+
+export async function userInfo(signer: ethers.Signer) {
+  try {
+    const signerAdress = await signer.getAddress();
+    const contract = await getContract(signer);
+    const userLocks = await contract?.getLockInfos(signerAdress);
+
+    let locks: LockInfo[] = [];
+    for (let i = 0; i < userLocks.length; i++) {
+      const userLock = userLocks[i];
+      const lock: LockInfo = {
+        lockIndex: i,
+        lockedAmount: Number(ethers.formatEther(userLock[1])),
+        withdrawnAmount: Number(ethers.formatEther(userLock[2])),
+        lockedAt: Number(userLock[3]),
+        unlockTime: Number(userLock[4]),
+        lockPeriod: Number(userLock[5]),
+        multiplier: Number(ethers.formatEther(userLock[6])),
+        points: Number(
+          ethers.formatEther(await contract?.pointsByLock(signerAdress, i))
+        ),
+      };
+
+      locks.push(lock);
+    }
+
+    const totalLocked: Number = locks.reduce((acc, curr) => {
+      return acc + curr.lockedAmount - curr.withdrawnAmount;
+    }, 0);
+
+    const totalPoints: Number = locks.reduce((acc, curr) => {
+      return acc + curr.points;
+    }, 0);
+
+    const samBalance = Number(
+      ethers.formatEther(
+        await balanceOf(ERC20_ABI, SAM_ADDRESS, signerAdress, signer)
+      )
+    );
+
+    return {
+      locks,
+      totalLocked,
+      totalPoints,
+      samBalance,
+    } as UserInfo;
+  } catch (e) {
+    handleError({ e: e, notificate: true });
+  }
+}
+
+export async function lock(
+  signer: ethers.Signer,
+  amount: string,
+  lockPeriod: number
+) {
+  try {
+    const signerAddress = await signer.getAddress();
+    const contract = await getContract(signer);
+    const network = await signer.provider?.getNetwork();
+
+    await checkApproval(
+      SAM_ADDRESS,
+      SAM_LOCK_ADDRESS,
+      signer,
+      ethers.parseEther(amount)
+    );
+
+    const tx = await contract?.lock(
+      signerAddress,
+      ethers.parseEther(amount),
+      lockPeriod
+    );
+
+    await notificateTx(tx, network);
+  } catch (e) {
+    handleError({ e: e, notificate: true });
+  }
+}
+
+export async function withdraw(
+  signer: ethers.Signer,
+  amount: string,
+  lockIndex: number
+) {
+  try {
+    const signerAddress = await signer.getAddress();
+    const contract = await getContract(signer);
+    const network = await signer.provider?.getNetwork();
+
+    const tx = await contract?.withdraw(
+      signerAddress,
+      ethers.parseEther(amount),
+      lockIndex.toString()
+    );
+
+    await notificateTx(tx, network);
+  } catch (e) {
+    handleError({ e: e, notificate: true });
+  }
+}
+
+export type Event = {
+  wallet: string;
+  amount: number;
+};
+
+export async function getLockedEvents() {
+  try {
+    const contract = await getContract();
+    const provider = new ethers.JsonRpcProvider(BASE_RPC_URL);
+    const startBlock = 13253153;
+    const currentBlock = await provider.getBlockNumber();
+    const blocksPerFilter = 10000;
+
+    let allEvents: Event[] = []; // Array to store all retrieved events
+
+    for (
+      let fromBlock = startBlock;
+      fromBlock <= currentBlock;
+      fromBlock += blocksPerFilter
+    ) {
+      const toBlock = Math.min(fromBlock + blocksPerFilter - 1, currentBlock); // Ensure toBlock doesn't exceed current block
+      const eventFilter = contract!.filters.Locked();
+      const events = await contract!.queryFilter(
+        eventFilter,
+        fromBlock,
+        toBlock
+      );
+
+      if (events && events?.length > 0) {
+        events.forEach(async (event: any) => {
+          const log = event.args as [string, number, number];
+          const wallet = log[0];
+
+          const index = allEvents.findIndex((event) => event.wallet === wallet);
+
+          if (index !== -1) {
+            allEvents[index].amount += Number(formatEther(log[1]));
+          } else {
+            allEvents.push({
+              wallet: log[0],
+              amount: Number(formatEther(log[1])),
+            });
+          }
+        });
+      }
+    }
+
+    return allEvents;
+  } catch (error) {
+    console.error("Error fetching contract log events:", error);
+    return [];
+  }
+}
+
+export type EventComplete = {
+  wallet: string;
+  amount: number;
+  points: number;
+};
+
+export async function getLockedCompleteInfos(lockedEvents: Event[]) {
+  try {
+    const contract = await getContract();
+    const completeEvents: EventComplete[] = [];
+
+    for (let i = 0; i < lockedEvents.length; i++) {
+      const element = lockedEvents[i];
+
+      const lockingsByWallet = await contract?.getLockInfos(element.wallet);
+      let pointsPerWallet = 0;
+
+      for (let ii = 0; ii < lockingsByWallet.length; ii++) {
+        const points = Number(
+          ethers.formatEther(await contract?.pointsByLock(element.wallet, ii))
+        );
+        pointsPerWallet += points;
+      }
+
+      completeEvents.push({
+        wallet: element.wallet,
+        amount: element.amount,
+        points: pointsPerWallet,
+      });
+    }
+
+    return completeEvents;
+  } catch (error) {
+    console.error("Error fetching contract log events:", error);
+    return [];
+  }
+}
+
+export async function getTotalLocked() {
+  try {
+    const lockedEvents = await getLockedEvents();
+
+    // CALL IT TO GET SUMARIZED VALUES ------------------------------------
+    // const completeEvents = await getLockedCompleteInfos(lockedEvents);
+    // console.log(completeEvents);
+    // --------------------------------------------------------------------
+
+    const totalLocked = lockedEvents?.reduce(
+      (acc: number, cur: Event) => acc + cur.amount,
+      0
+    );
+
+    return totalLocked;
+  } catch (error) {
+    return 0;
+  }
+}
+
+export async function downloadPointsFile() {
+  const lockedEvents = await getLockedEvents();
+
+  const completeEvents = await getLockedCompleteInfos(lockedEvents);
+
+  // Convert completeEvents to a JSON string
+  const jsonData = JSON.stringify(completeEvents, null, 2); // Optional: Indent for readability
+
+  // Create a Blob object with the JSON data and appropriate content type
+  const blob = new Blob([jsonData], { type: "application/json" });
+
+  return blob;
+}
+
+// ADMIN ACTIONS
+
+export async function togglePause(index: number, signer: ethers.Signer) {
+  const contract = await getContract(signer);
+
+  try {
+    const owner = await contract?.owner();
+    const signerAddress = await signer.getAddress();
+
+    if (owner === signerAddress) {
+      const network = await signer.provider?.getNetwork();
+      const isPaused = await contract?.paused();
+      const tx = isPaused ? await contract?.unpause() : await contract?.pause();
+      await notificateTx(tx, network);
+    }
+  } catch (e) {
+    handleError({ e: e, notificate: true });
+  }
+}
